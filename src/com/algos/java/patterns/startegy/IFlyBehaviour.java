d728108307c114b28513d59339d9f537c2bd3f5c
Chandrachudeswaran <chandrachudeswaran@gmail.com>
2016-01-14 11:20:47 -0500

    Moved the design patterns to a new repo
    Moved the design patterns to a new repo

--- src/com/algos/java/patterns/startegy/IFlyBehaviour.java
+++
@@ -1,8 +0,0 @@
-package com.algos.java.patterns.startegy;
-
-//Interface to be implemented for defining a new fly behaviour for Ducks
-public interface IFlyBehaviour {
-	
-	public void fly();
-
-}
--- src/com/algos/java/patterns/startegy/IQuackBehaviour.java
+++
@@ -1,8 +0,0 @@
-package com.algos.java.patterns.startegy;
-
-//Interface to be implemented for defining a new quack behaviour for Ducks
-public interface IQuackBehaviour {
-	
-	public void quack();
-
-}
